470d1ca3feb6a1bce51c46040f2376a862e9558d
petrplot <petr.plotnikov.1989@mail.ru>
2023-04-09 15:34:09 +0700

    added several routes

--- server/src/routes/categoriesRout.js
+++ server/src/routes/categoriesRout.js
@@ -3,8 +3,7 @@ const router = express.Router()
 
 router.get('/')
 router.get('/:id')
-router.post('/login')
-router.post('/reg')
+router.post('/')
 router.delete('/:id')
 router.put('/:id')
 
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,12 @@
-const Router = require ('express')
-const router = new Router()
+const express = require('express') 
+const router = express.Router()
 const userRout = require ('./userRout.js')
+const productRout = require('./productRout.js')
+const categoriesRout = require('./categoriesRout.js')
 
 
 router.use('/user', userRout)
+router.use('/product', productRout)
+router.use('/categories', categoriesRout)
 
 module.exports = router
